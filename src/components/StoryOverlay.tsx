02dcf1802d2c2d622a2f2c367a171f3be471f2a9
noctua <20678151+n0ctua@users.noreply.github.com>
2026-08-28 07:35:19 +0200

    ✏️ Update FAQ URL

--- src/components/StoryOverlay.tsx
+++ src/components/StoryOverlay.tsx
@@ -36,7 +36,7 @@ type Props = {
   canGoPrev: boolean;
 };
 
-const FAQ_HREF = "https://fragdenstaat.de/aktionen/heisse-luft/faq/";
+const FAQ_HREF = "fragdenstaat.de/aktionen/heisse-luft/heisseluft-faq-und-methodik/";
 
 const TEXT: Partial<Record<SceneId, StoryTextEntry>> = {
   intro: {
